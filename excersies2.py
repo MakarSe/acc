e38aeae2ab7a76c54d04dd83395d02945e64f2db
MakarSe <92166903+MakarSe@users.noreply.github.com>
2022-10-17 12:08:50 +0100

    Add files via upload

---
+++ excersies2.py
@@ -0,0 +1,95 @@
+#1
+n1 = float(input("Enter a number: "))
+n2 = float(input("Enter a second number: "))
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+
+#2
+n3 = float(input("Enter a 5 letter number: "))
+
+m1 = round(n3 / 60)
+print(m1)
+
+#3
+n4 = str(input("Enter a number 1: "))
+n5 = str(input("Enter a number 2: "))
+n6 = str(input("Enter a number 3: "))
+n7 = str(input("Enter a number 4: "))
+n8 = str(input("Enter a number 5: "))
+
+print(ord(n4))
+print(ord(n5))
+print(ord(n6))
+print(ord(n7))
+print(ord(n8))
+
+#4
+n9 = int(input("Enter a value: "))
+counter = 0
+total = 0
+while n9 > 0:
+    total = total + n9
+    counter = counter + 1
+    
+    n9 = int(input("Enter value: "))
+    
+avg = total/counter
+    
+print(avg)
+
+#5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c = 0
+total = 0
+while n9 > 0:
+    total = total + n9
+    counter = n9 + 1
+    
+    n9 = int(input("Enter a value: "))
+    avg = n9 / c
+
+print (avg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
